1ae532df769f938498e05116d34cabb881daea0c
Jeffrey Wallace <jeffrey.wallace81@gmail.com>
2023-09-07 16:00:52 -0500

    Initial setup to point at the rails server and display something on the screen

---
+++ src/components/LocationsContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class LocationsContainer extends Component {
+  render() {
+    return (
+      <div>
+        <div className="wrapItems">
+          <ul className="listItems"></ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LocationsContainer;
